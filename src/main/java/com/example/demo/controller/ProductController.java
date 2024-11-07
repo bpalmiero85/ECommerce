5bd3e149da3fa5ac8cb394d627e7a945ddcdecf4
bpalmiero85 <bpalmiero85@gmail.com>
2024-11-06 21:36:41 -0500

    Controller functions working

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,67 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+  private final ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @PostMapping("/product")
+  public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+    Product newProduct = productService.saveProduct(product);
+    return ResponseEntity.ok(newProduct);
+  }
+
+  @GetMapping("/products")
+  public List<Product> getAllProducts() {
+    return productService.getAllProducts();
+  }
+
+  @GetMapping("/products/{id}")
+  public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+    Optional<Product> product = productService.getProductById(id);
+    return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("/products/{id}")
+  public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+    Product updatedProduct = productService.updateProduct(id, product);
+    return ResponseEntity.ok(updatedProduct);
+  }
+
+  @DeleteMapping("/products/{id}")
+  public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+    Optional<Product> product = productService.getProductById(id);
+
+    if (product.isPresent()) {
+      productService.deleteProduct(id);
+      System.out.println("Product deleted successfully");
+      return ResponseEntity.noContent().build();
+
+    } else {
+      System.out.println("There is no product with that id");
+      return ResponseEntity.notFound().build();
+    }
+  }
+}
---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+  private final ProductRepository productRepository;
+
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  public Product saveProduct(Product product) {
+    return productRepository.save(product);
+  }
+
+  public List<Product> getAllProducts() {
+    return productRepository.findAll();
+  }
+
+  public Optional<Product> getProductById(Long id) {
+    return productRepository.findById(id);
+  }
+
+  public Product updateProduct(Long id, Product updatedProduct) {
+    Optional<Product> existingProduct = productRepository.findById(id);
+    if(existingProduct.isPresent()) {
+      Product product = existingProduct.get();
+      product.setName(updatedProduct.getName());
+      product.setPrice(updatedProduct.getPrice());
+      product.setQuantity(updatedProduct.getQuantity());
+      return productRepository.save(product);
+    } else {
+      throw new RuntimeException("Product not found");
+    }
+  }
+
+  public void deleteProduct(Long id) {
+    if (productRepository.existsById(id)) {
+      productRepository.deleteById(id);
+    } else {
+      throw new IllegalArgumentException("Product with id " + id + " does not exist");
+    }
+    
+  }
+}
